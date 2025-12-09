eaaf95cb93f68263f7f6c7e745bfe1dd20d6f8bf
Wei <liwei810812@gmail.com>
2025-12-09 13:49:39 +0100

    fix paths

--- genfragments/OO_5p36TeV/Starlight/STARLIGHT_incoh_jpsi_diel_pmin0-fragment.py
+++ genfragments/OO_5p36TeV/Starlight/STARLIGHT_incoh_jpsi_diel_pmin0-fragment.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
-    args = cms.vstring('/cvmfs/cms-griddata.cern.ch/phys_generator/gridpacks_tarball/OO/5p36TeV/starlight/incoherent_jpsiToEE/starlight_incoherent_jpsi_dielectron_el8_amd64_gcc12_CMSSW_15_0_11_tarball.tgz'),
+    args = cms.vstring('/cvmfs/cms-griddata.cern.ch/phys_generator/gridpacks_tarball/OO/5p36TeV/starlight/incoherent_JpsiToEE/starlight_incoherent_jpsi_dielectron_el8_amd64_gcc12_CMSSW_15_0_11_tarball.tgz'),
     nEvents = cms.untracked.uint32(1),
     numberOfParameters = cms.uint32(1),
     outputFile = cms.string('cmsgrid_final.lhe'),
